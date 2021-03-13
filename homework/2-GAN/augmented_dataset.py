d8d91ee2d642d88f57868c8cda8d8b55e114d6eb
georgepsh <georgii39@gmail.com>
2021-03-13 22:49:31 +0000

    dataset fix

--- homework/2-GAN/augmented_dataset.py
+++ homework/2-GAN/augmented_dataset.py
@@ -3,13 +3,13 @@ from torch.utils.data import Dataset
 
 class AugmentedDataset(Dataset):
     def __init__(self, dataset, transform):
-        self.dateset = dataset
+        self.data = dataset.data
         self.transform = transform
     
     def __len__(self):
         return self.data.shape[0]
     
     def __getitem__(self, idx):
-        image, label = self.dataset[idx]
+        image, label = self.data[idx]
         image = self.transform(image)
         return (image, label)
